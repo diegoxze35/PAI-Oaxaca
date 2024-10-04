cfa1a3ab9d072d18a81386e642b957ca9524bb9e
MetztliAtzintli <ulises.mota.17@gmail.com>
2024-10-03 20:14:28 -0600

    TOKEN UPDATED

--- index/js/api/apiCaller.js
+++ index/js/api/apiCaller.js
@@ -1,4 +1,4 @@
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRtb3Jlbm9tMjAwMkBhbHVtbm8uaXBuLm14Iiwicm9sZSI6IkV4dGVybm9zIiwibmJmIjoxNzI3ODMzNTU4LCJleHAiOjE3Mjc5MTk5NTgsImlhdCI6MTcyNzgzMzU1OCwiaXNzIjoic2VsZiIsImF1ZCI6ImxvY2FsaG9zdDo1NzI1MiJ9.84qq42sU52AaahAcNQa3WN7obzTimRQrSkDCPY3j6h8";
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImRtb3Jlbm9tMjAwMkBhbHVtbm8uaXBuLm14Iiwicm9sZSI6IkV4dGVybm9zIiwibmJmIjoxNzI4MDA4MDU3LCJleHAiOjE3MjgwOTQ0NTcsImlhdCI6MTcyODAwODA1NywiaXNzIjoic2VsZiIsImF1ZCI6ImxvY2FsaG9zdDo1NzI1MiJ9.RI-kghRkLjmJu-XyNFLgGL8MNVx295FD4lS562sRCs0";
 const URL = "https://sistemas.coneval.org.mx/SIID/api/V1/publico/pobreza";
 export async function getPovertyConditionByIdAndYear(id, year=2020) {
     const paramethers = `/${year}/0/20/${id}/2`;
